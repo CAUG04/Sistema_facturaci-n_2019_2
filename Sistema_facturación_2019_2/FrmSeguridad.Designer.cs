5c0da52ea1fa8cd00d665c0edcaef8a8825cd826
Carlos Urrego <carlos.urrego@tekus.co>
2019-08-25 19:33:56 -0500

    Se agrega formulario de seguridad del menú seguridad

---
+++ Sistema_facturación_2019_2/FrmSeguridad.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Sistema_facturación_2019_2
+{
+    partial class FrmSeguridad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmpleadoSeguridad = new MaterialSkin.Controls.MaterialLabel();
+            this.cboEmpleadoSeguridad = new System.Windows.Forms.ComboBox();
+            this.BtnSalirSeguridad = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.BtnEliminarEmpleadoSeguridad = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnActualizarEmpleadoSeguridad = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.BtnNuevoEmpleadoSeguridad = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.TxtClaveSeguridad = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtUsuarioSeguridad = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.lblSeguridad = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // lblEmpleadoSeguridad
+            // 
+            this.lblEmpleadoSeguridad.AutoSize = true;
+            this.lblEmpleadoSeguridad.Depth = 0;
+            this.lblEmpleadoSeguridad.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblEmpleadoSeguridad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblEmpleadoSeguridad.Location = new System.Drawing.Point(544, 88);
+            this.lblEmpleadoSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblEmpleadoSeguridad.Name = "lblEmpleadoSeguridad";
+            this.lblEmpleadoSeguridad.Size = new System.Drawing.Size(76, 19);
+            this.lblEmpleadoSeguridad.TabIndex = 55;
+            this.lblEmpleadoSeguridad.Text = "Empleado";
+            // 
+            // cboEmpleadoSeguridad
+            // 
+            this.cboEmpleadoSeguridad.FormattingEnabled = true;
+            this.cboEmpleadoSeguridad.Location = new System.Drawing.Point(660, 89);
+            this.cboEmpleadoSeguridad.Name = "cboEmpleadoSeguridad";
+            this.cboEmpleadoSeguridad.Size = new System.Drawing.Size(144, 21);
+            this.cboEmpleadoSeguridad.TabIndex = 54;
+            // 
+            // BtnSalirSeguridad
+            // 
+            this.BtnSalirSeguridad.Depth = 0;
+            this.BtnSalirSeguridad.Location = new System.Drawing.Point(855, 409);
+            this.BtnSalirSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnSalirSeguridad.Name = "BtnSalirSeguridad";
+            this.BtnSalirSeguridad.Primary = true;
+            this.BtnSalirSeguridad.Size = new System.Drawing.Size(86, 24);
+            this.BtnSalirSeguridad.TabIndex = 53;
+            this.BtnSalirSeguridad.Text = "Salir";
+            this.BtnSalirSeguridad.UseVisualStyleBackColor = true;
+            this.BtnSalirSeguridad.Click += new System.EventHandler(this.BtnSalirSeguridad_Click);
+            // 
+            // BtnEliminarEmpleadoSeguridad
+            // 
+            this.BtnEliminarEmpleadoSeguridad.Depth = 0;
+            this.BtnEliminarEmpleadoSeguridad.Location = new System.Drawing.Point(855, 172);
+            this.BtnEliminarEmpleadoSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnEliminarEmpleadoSeguridad.Name = "BtnEliminarEmpleadoSeguridad";
+            this.BtnEliminarEmpleadoSeguridad.Primary = true;
+            this.BtnEliminarEmpleadoSeguridad.Size = new System.Drawing.Size(86, 23);
+            this.BtnEliminarEmpleadoSeguridad.TabIndex = 52;
+            this.BtnEliminarEmpleadoSeguridad.Text = "Eliminar";
+            this.BtnEliminarEmpleadoSeguridad.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizarEmpleadoSeguridad
+            // 
+            this.btnActualizarEmpleadoSeguridad.Depth = 0;
+            this.btnActualizarEmpleadoSeguridad.Location = new System.Drawing.Point(855, 124);
+            this.btnActualizarEmpleadoSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnActualizarEmpleadoSeguridad.Name = "btnActualizarEmpleadoSeguridad";
+            this.btnActualizarEmpleadoSeguridad.Primary = true;
+            this.btnActualizarEmpleadoSeguridad.Size = new System.Drawing.Size(86, 23);
+            this.btnActualizarEmpleadoSeguridad.TabIndex = 51;
+            this.btnActualizarEmpleadoSeguridad.Text = "Actualizar";
+            this.btnActualizarEmpleadoSeguridad.UseVisualStyleBackColor = true;
+            // 
+            // BtnNuevoEmpleadoSeguridad
+            // 
+            this.BtnNuevoEmpleadoSeguridad.Depth = 0;
+            this.BtnNuevoEmpleadoSeguridad.Location = new System.Drawing.Point(855, 83);
+            this.BtnNuevoEmpleadoSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnNuevoEmpleadoSeguridad.Name = "BtnNuevoEmpleadoSeguridad";
+            this.BtnNuevoEmpleadoSeguridad.Primary = true;
+            this.BtnNuevoEmpleadoSeguridad.Size = new System.Drawing.Size(86, 24);
+            this.BtnNuevoEmpleadoSeguridad.TabIndex = 50;
+            this.BtnNuevoEmpleadoSeguridad.Text = "Nuevo";
+            this.BtnNuevoEmpleadoSeguridad.UseVisualStyleBackColor = true;
+            // 
+            // TxtClaveSeguridad
+            // 
+            this.TxtClaveSeguridad.Depth = 0;
+            this.TxtClaveSeguridad.Hint = "";
+            this.TxtClaveSeguridad.Location = new System.Drawing.Point(8, 124);
+            this.TxtClaveSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TxtClaveSeguridad.Name = "TxtClaveSeguridad";
+            this.TxtClaveSeguridad.PasswordChar = '\0';
+            this.TxtClaveSeguridad.SelectedText = "";
+            this.TxtClaveSeguridad.SelectionLength = 0;
+            this.TxtClaveSeguridad.SelectionStart = 0;
+            this.TxtClaveSeguridad.Size = new System.Drawing.Size(432, 23);
+            this.TxtClaveSeguridad.TabIndex = 49;
+            this.TxtClaveSeguridad.Text = "Clave";
+            this.TxtClaveSeguridad.UseSystemPasswordChar = false;
+            // 
+            // txtUsuarioSeguridad
+            // 
+            this.txtUsuarioSeguridad.Depth = 0;
+            this.txtUsuarioSeguridad.Hint = "";
+            this.txtUsuarioSeguridad.Location = new System.Drawing.Point(8, 84);
+            this.txtUsuarioSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtUsuarioSeguridad.Name = "txtUsuarioSeguridad";
+            this.txtUsuarioSeguridad.PasswordChar = '\0';
+            this.txtUsuarioSeguridad.SelectedText = "";
+            this.txtUsuarioSeguridad.SelectionLength = 0;
+            this.txtUsuarioSeguridad.SelectionStart = 0;
+            this.txtUsuarioSeguridad.Size = new System.Drawing.Size(432, 23);
+            this.txtUsuarioSeguridad.TabIndex = 48;
+            this.txtUsuarioSeguridad.Text = "Usuario";
+            this.txtUsuarioSeguridad.UseSystemPasswordChar = false;
+            // 
+            // lblSeguridad
+            // 
+            this.lblSeguridad.AutoSize = true;
+            this.lblSeguridad.Depth = 0;
+            this.lblSeguridad.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblSeguridad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblSeguridad.Location = new System.Drawing.Point(387, 23);
+            this.lblSeguridad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblSeguridad.Name = "lblSeguridad";
+            this.lblSeguridad.Size = new System.Drawing.Size(90, 19);
+            this.lblSeguridad.TabIndex = 47;
+            this.lblSeguridad.Text = "SEGURIDAD";
+            // 
+            // FrmSeguridad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 558);
+            this.Controls.Add(this.lblEmpleadoSeguridad);
+            this.Controls.Add(this.cboEmpleadoSeguridad);
+            this.Controls.Add(this.BtnSalirSeguridad);
+            this.Controls.Add(this.BtnEliminarEmpleadoSeguridad);
+            this.Controls.Add(this.btnActualizarEmpleadoSeguridad);
+            this.Controls.Add(this.BtnNuevoEmpleadoSeguridad);
+            this.Controls.Add(this.TxtClaveSeguridad);
+            this.Controls.Add(this.txtUsuarioSeguridad);
+            this.Controls.Add(this.lblSeguridad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmSeguridad";
+            this.Text = "FrmSeguridad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel lblEmpleadoSeguridad;
+        private System.Windows.Forms.ComboBox cboEmpleadoSeguridad;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnSalirSeguridad;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnEliminarEmpleadoSeguridad;
+        private MaterialSkin.Controls.MaterialRaisedButton btnActualizarEmpleadoSeguridad;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnNuevoEmpleadoSeguridad;
+        private MaterialSkin.Controls.MaterialSingleLineTextField TxtClaveSeguridad;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtUsuarioSeguridad;
+        private MaterialSkin.Controls.MaterialLabel lblSeguridad;
+    }
+}
