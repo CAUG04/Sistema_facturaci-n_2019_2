69be8e8fe763202d0ed94033951f801fe9791816
Carlos Urrego <carlos.urrego@tekus.co>
2019-09-28 22:36:13 -0500

    Se crea la clase Acceso_datos en la que agregamos la cadena de conexión para la base de datos de DBFACTURACION

---
+++ Sistema_facturación_2019_2/Acceso_datos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;// se utiliza inicialmente para mostrar los mensajes, a future no la utilizamos ya que la clase no deberia mostrar los mensajes
+
+namespace Sistema_facturación_2019_2
+{
+    class Acceso_datos
+    {
+        SqlConnection conexion; // se define la variable para la conexión de tipo SqlConnection
+        SqlCommand cmd; // se defie la variable para realizar comandos en la BD
+        SqlDataReader LectorDatos = null;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+        DataTable dt;
+        DataSet ds;
+        public void AbrirBd() // método para abrir la base de datos
+        {
+            try // permite captura de un error en caso que se presente
+            {
+                // creamos un objeto de tipo conexión a la base de datos y se pasa como parámetro la cadena de conexión
+                conexion = new SqlConnection(@"Data Source=CAUG04\CAUG; Database=DBFACTURAS;Integrated Security=True");
+                conexion.Open(); // invocamos método para abrir la base de datos
+            }
+            catch (Exception ex)// si hay error presenta el siguiente mensaje
+            {
+                MessageBox.Show("falló conexión " + ex);
+            }
+        }
+        public void CerrarrBd() // método para cerrar la base de datos
+        {
+            try // permite captura de un error en caso que se presente
+            {
+                conexion.Close(); // invocamos método para cerrar la base de datos
+            }
+            catch (Exception ex)// si hay error presenta el siguiente mensaje
+            {
+                MessageBox.Show("falló cerrar conexión " + ex);
+            }
+        }
+
+        // método para validar el ingreso del usuario al sistema
+        public string ValidarUsuario(string StrUsuario, string StrClave)
+        {
+            try
+            {
+                string StrEmpleado = "";
+                string sentencia = $"select e.strNombre, e.IdRolEmpleado from TBLSEGURIDAD s JOIN TBLEMPLEADO e ON s.IdEmpleado = e.IdEmpleado where StrUsuario = '{StrUsuario}' and StrClave = '{StrClave}'";
+                AbrirBd();
+                cmd = new SqlCommand();
+                // utilizamos las propiedades de SqlCommand
+                cmd.Connection = conexion;
+                cmd.CommandText = sentencia;
+                cmd.CommandType = CommandType.Text; // otros tipos son :CommandType.StoredProcedure CommandType.TableDirect
+                cmd.CommandTimeout = 10;
+                LectorDatos = cmd.ExecuteReader();
+                while (LectorDatos.Read())
+                {
+                    StrEmpleado = Convert.ToString(LectorDatos.GetValue(0));
+                }
+                if (LectorDatos != null) // cerramos el lector de datos
+                {
+                    LectorDatos.Close();
+                }
+                return StrEmpleado;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("FALLA LECTURA: " + ex);
+                return "";
+            }
+        }
+    }
+}
+  
--- Sistema_facturación_2019_2/Form1.cs
+++ Sistema_facturación_2019_2/Form1.cs
@@ -24,10 +24,36 @@ namespace Sistema_facturación_2019_2
 
         private void btnValidar_Click(object sender, EventArgs e)
         {
-            // Se crea el objeto del formulario principal
-            FrmPrincipal frmprincipal = new FrmPrincipal();
-            this.Hide(); // oculta el formulario login
-            frmprincipal.Show(); // muestra el formulario principal
+            //// Se crea el objeto del formulario principal
+            //FrmPrincipal frmprincipal = new FrmPrincipal();
+            //this.Hide(); // oculta el formulario login
+            //frmprincipal.Show(); // muestra el formulario principal
+
+            string Respuesta = ""; // creamos variable para controlar si encontró el usuario en la base de datos
+            if (TxtUsuario.Text != "" && TxtPassword.Text != string.Empty) // verifico que el usuario y la clave del los campos del formulario no estén vacíos
+            {
+                Acceso_datos Acceso = new Acceso_datos(); // creamos un objeto con la clase Acceso_datos
+                Respuesta = Acceso.ValidarUsuario(TxtUsuario.Text, TxtPassword.Text);
+                if (Respuesta != "")
+                {
+                    MessageBox.Show("Bienvenido : " + Respuesta);
+                    FrmPrincipal frmppal = new FrmPrincipal(); //Creamos el objeto del formulario FrmPrincipal
+                    this.Hide(); // Ocultamos el formulario login
+                    frmppal.Show(); // Mostramos el formulario principal
+                }
+                else
+                {
+                    MessageBox.Show("USUARIOS Y CLAVE NO ENCONTRADOS");
+                    TxtUsuario.Text = "";
+                    TxtUsuario.Focus();
+                    TxtPassword.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debes ingresar un usuario y una clave");
+            }
         }
     }
 }
+
